35f1036007f41b8e927d9e04963a32e1e84ec9c4
Gold Gray <goldgray8888@gmail.com>
2025-09-03 21:29:04 +0800

    animation

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ document.querySelectorAll('.video-card iframe').forEach(function(iframe) {
                 iframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
             });
         });
+
+// animations
+document.addEventListener("DOMContentLoaded", function() {
+        const fadeSection = document.querySelector('.fade-in-section3');
+        function handleFadeIn(entries, observer) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('visible');
+                    observer.unobserve(entry.target);
+                }
+            });
+        }
+        const observer = new IntersectionObserver(handleFadeIn, { threshold: 0.2 });
+        if (fadeSection) observer.observe(fadeSection);
+    });
